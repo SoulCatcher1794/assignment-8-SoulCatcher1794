016343783a04fe2a863fd0130716a39dd37a09da
Alan Cano <alan.canoduran@colorado.edu>
2025-11-18 18:41:44 -0600

    Assignment 4, part 2 completed, files updated

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cdd0256a2620a901331c94d990b227f496566388'
+AESD_ASSIGNMENTS_VERSION = 3fe6893c3b4cbd003d135b41be05fed484da572f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-SoulCatcher1794.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-SoulCatcher1794.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
