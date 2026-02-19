6f07dfe97f270ce8f8144f153346a02379745cf4
Alan Cano <cano.alan1794@gmail.com>
2026-02-18 20:30:41 -0600

    shared.sh modified to include buildroot download directory and enable compiler cache

--- shared.sh
+++ shared.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # Shared definitions for buildroot scripts
 
+# Shared download directory for Buildroot packages
+export BR2_DL_DIR=${HOME}/.dl
+mkdir -p ${BR2_DL_DIR}
+
+# Enable compiler cache to speed up rebuilds
+export BR2_CCACHE=y
+export BR2_CCACHE_DIR=${HOME}/.buildroot-ccache
+mkdir -p ${BR2_CCACHE_DIR}
+
+# Set ccache size limit (default is 1GB, increase for larger projects)
+export BR2_CCACHE_SIZE=5G
+
 # The defconfig from the buildroot directory we use for qemu builds
 QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
 # The place we store customizations to the qemu configuration
